aad26fd68ce6a2cbe7f6707eb716b55ee0835581
CA GG <cagalvangrajales@gmail.com>
2019-03-22 18:01:42 +0100

    ~0.0.2 ready for send message, just waiting for an email different than gmail

--- web/src/api.js
+++ web/src/api.js
@@ -2,9 +2,38 @@
 
 const express = require('express')
 const chalk = require('chalk')
+const nodemailer = require('nodemailer')
 const api = express.Router()
 
 api.get('/', (req, res, next) => {
+  return res.send('aqui esta el home!')
+})
+
+api.get('/send/:message', (req, res, next) => {
+  const { message } = req.params
+  console.log(message)
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'cagalvangrajales@gmail.com',
+      pass: 'macasu123fuckyou'
+    }
+  })
+  
+  var mailOptions = {
+    from: 'ca@gmail.com',
+    to: 'carlos13arturo09@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: message
+  }
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error)
+    } else {
+      console.log('Email sent: ' + info.response)
+    }
+  })
   return res.send('aqui esta el home!')
 })
 
